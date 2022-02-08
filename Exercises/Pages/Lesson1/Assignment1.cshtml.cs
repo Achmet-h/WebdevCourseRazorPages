be95305e83fd9001e634710bf974021d0ac7e8fb
Achmet-h <91320230+Achmet-h@users.noreply.github.com>
2022-02-08 15:32:37 +0100

    test

--- Exercises/Pages/Lesson1/Assignment1.cshtml.cs
+++ Exercises/Pages/Lesson1/Assignment1.cshtml.cs
@@ -1,11 +1,63 @@
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Exercises.Pages.Lesson1
 {
     public class Assignment1 : PageModel
     {
+        [BindProperty(SupportsGet = true, Name = "scoreHome")] 
+        public int ScoreHome { get; set; } = 0;
+        [BindProperty(SupportsGet = true, Name = "scoreAway")] 
+        public int ScoreAway { get; set; } = 0;
+         
         public void OnGet()
         {
+            if (Request.Query.ContainsKey("incrementHome"))
+            {
+                ScoreHome = Convert.ToInt32(Request.Query["incrementHome"].First());
+                ScoreHome++;
+            }
+
+            if (Request.Query.ContainsKey("decrementHome"))
+            {
+                ScoreHome = Convert.ToInt32(Request.Query["decrementHome"].First());
+                if (ScoreHome==0)
+                {
+                    ScoreHome = 0;
+                }
+                else
+                {
+                    ScoreHome--;
+                }
+            
+            }
+            if (Request.Query.ContainsKey("incrementAway"))
+            {
+                ScoreAway = Convert.ToInt32(Request.Query["incrementAway"].First());
+                ScoreAway++;
+            } 
+            if (Request.Query.ContainsKey("decrementAway"))
+            {
+                ScoreAway = Convert.ToInt32(Request.Query["decrementAway"].First());
+                if (ScoreAway==0)
+                {
+                    ScoreHome = 0;
+                }
+                else
+                {
+                    ScoreAway--;
+                }
+            }
+
+            if (Request.Query.ContainsKey("Reset"))
+            {
+                ScoreAway = 0;
+                ScoreHome = 0;
+            }
+        
+
         }
     }
 }
